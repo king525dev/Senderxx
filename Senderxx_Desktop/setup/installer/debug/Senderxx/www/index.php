0e524fe01b8627c8cf5821faf808f00eaa17cd57
king525dev <109004601+king525dev@users.noreply.github.com>
2022-08-15 00:32:22 +0100

    Uploaded Senderxx Desktop

---
+++ Senderxx_Desktop/setup/installer/debug/Senderxx/www/index.php
@@ -0,0 +1,15 @@
+<html>
+     <head>
+          <title>Loading Senderxx....</title>
+          <meta charset="UTF-8">
+          <meta http-equiv="refresh"
+               content="5 ; url='https://sndchatapp.000webhostapp.com/index.php'">
+          <link rel="stylesheet" href="loading.css">
+     </head>
+     <body>
+          <div class="loading">
+               <span>Loading</span>
+          </div>
+
+     </body>
+</html>
